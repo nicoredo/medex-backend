65d5c52beb501a6c39f391d706ae3de3e564c617
nicoredo <nicolas.redo@gmail.com>
2025-06-29 02:37:06 -0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://medex.ar","https://medex.ar"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
